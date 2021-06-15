a92baa4b4eb7269fd92c0d9c0ca09609ce947084
Denis Kravchu <denkravchu@gmail.com>
2021-06-15 19:19:28 +0200

    (1)

--- js/swipe.js
+++ js/swipe.js
@@ -11,7 +11,7 @@ window.addEventListener('load', function() {
       readyToSwipe = false
       setTimeout(function() {
         readyToSwipe = true
-      }, 400)
+      }, 0)
 
       switch ( direction ) {
         case 'left':
@@ -58,6 +58,8 @@ function detectSwipe(id, func, deltaMin = 90) {
     const t = e.touches[0]
     swipe_det.sX = t.screenX
     swipe_det.sY = t.screenY
+    swipe_det.eX = swipe_det.sX
+    swipe_det.eY = swipe_det.sY
   }, false)
   el.addEventListener('touchmove', function(e) {
     // Prevent default will stop user from scrolling, use with care
@@ -69,14 +71,15 @@ function detectSwipe(id, func, deltaMin = 90) {
   el.addEventListener('touchend', function(e) {
     const deltaX = swipe_det.eX - swipe_det.sX
     const deltaY = swipe_det.eY - swipe_det.sY
+
     // Min swipe distance, you could use absolute value rather
     // than square. It just felt better for personnal use
     if (deltaX ** 2 + deltaY ** 2 < deltaMin ** 2) return
     // horizontal
     if (deltaY === 0 || Math.abs(deltaX / deltaY) > 1)
-      direction = deltaX > 0 ? directions.RIGHT : directions.LEFT
+      direction = deltaX > 90 ? directions.RIGHT : directions.LEFT
     else // vertical
-      direction = deltaY > 0 ? directions.UP : directions.DOWN
+      direction = deltaY > 90 ? directions.UP : directions.DOWN
 
     if (direction && typeof func === 'function') func(el, direction)
 
