7d7ec0e5e0a46c32a7968e07e880e132e38a0db3
Denis Kravchu <denkravchu@gmail.com>
2021-06-16 13:18:23 +0200

    (30)

--- js/swipe.js
+++ js/swipe.js
@@ -1,17 +1,20 @@
 window.addEventListener('load', function() {
   // Переключение планет на главном экране -------------------------
+  const planets = ['earth', 'moon', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'sun', 'mercury', 'venus']
   const mobileCarousel = {
-    planets: ['earth', 'moon', 'mars', 'jupiter', 'saturn', 'uranus', 'neptune', 'sun', 'mercury', 'venus'],
-    activeIndex: 0
+    planets: planets,
+    previousIndex: planets.length - 1,
+    activeIndex: 0,
+    nextIndex: 1
   }
   let readyToSwipe = true
-  detectSwipe('swipe', (root, direction) => {
+  detectSwipe('swipe', mobileCarousel, (root, direction) => {
     if (root.classList.contains('zoom-out') && window.outerWidth < 799 && readyToSwipe) {
 
       readyToSwipe = false
       setTimeout(function() {
         readyToSwipe = true
-      }, 0)
+      }, 100)
 
       switch ( direction ) {
         case 'left':
@@ -26,19 +29,27 @@ window.addEventListener('load', function() {
 })
 
 function swipePlanet(root, mobileCarousel, step) {
-  const previousIndex = mobileCarousel.activeIndex
+  const previousPlanet_Index = mobileCarousel.activeIndex
   let newIndex = mobileCarousel.activeIndex + step
   if ( newIndex < 0 ) newIndex = mobileCarousel.planets.length - 1
   if ( newIndex === mobileCarousel.planets.length ) newIndex = 0
+  let prevIndex = newIndex - 1
+  let nextIndex = newIndex + 1
+  if ( prevIndex < 0 ) prevIndex = mobileCarousel.planets.length - 1
+  if ( nextIndex === mobileCarousel.planets.length ) nextIndex = 0
+  mobileCarousel.previousIndex = prevIndex
   mobileCarousel.activeIndex = newIndex
+  mobileCarousel.nextIndex = nextIndex
 
-  const previousPlanetName = mobileCarousel.planets[previousIndex]
+  console.log(mobileCarousel)
+
+  const previousPlanetName = mobileCarousel.planets[previousPlanet_Index]
   const newPlanetName = mobileCarousel.planets[newIndex]
 
   root.classList.add('planet-mobile-active-' + newPlanetName)
   root.classList.remove('planet-mobile-active-' + previousPlanetName)
 }
-function detectSwipe(id, func, deltaMin = 90) {
+function detectSwipe(id, mobileCarousel, func, deltaMin = 100) {
   const swipe_det = {
     sX: 0,
     sY: 0,
@@ -60,6 +71,7 @@ function detectSwipe(id, func, deltaMin = 90) {
     swipe_det.sY = t.screenY
     swipe_det.eX = swipe_det.sX
     swipe_det.eY = swipe_det.sY
+
   }, false)
   el.addEventListener('touchmove', function(e) {
     // Prevent default will stop user from scrolling, use with care
@@ -67,8 +79,42 @@ function detectSwipe(id, func, deltaMin = 90) {
     const t = e.touches[0]
     swipe_det.eX = t.screenX
     swipe_det.eY = t.screenY
+
+    // Показываем, куда свайпать будет
+    const deltaSkewX = swipe_det.eX - swipe_det.sX
+    const deltaSkewY = swipe_det.eY - swipe_det.sY
+    if (deltaSkewY === 0 || Math.abs(deltaSkewX / deltaSkewY) > 1) {
+      const skewActivePlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' .card-planet')
+      const skewPrevPlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' .card-planet')
+      const skewNextPlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' .card-planet')
+      skewActivePlanetCard.style.cssText = `transform: skew(${deltaSkewX / 50}deg) translateX(${deltaSkewX / 5}px); transition: 0s;`
+      skewPrevPlanetCard.style.cssText = `transform: skew(${deltaSkewX / 80}deg) translateX(${deltaSkewX / 10}px); transition: 0s;`
+      skewNextPlanetCard.style.cssText = `transform: skew(${deltaSkewX / 80}deg) translateX(${deltaSkewX / 10}px); transition: 0s;`
+
+      const skewActivePlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' video')
+      const skewPrevPlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' video')
+      const skewNextPlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' video')
+      skewActivePlanet.style.cssText = `transform: translateX(${deltaSkewX / 8}px); transition: 0s;`
+      skewPrevPlanet.style.cssText = `transform: translateX(${deltaSkewX / 14}px); transition: 0s;`
+      skewNextPlanet.style.cssText = `transform: translateX(${deltaSkewX / 14}px); transition: 0s;`
+
+    }
+    //
   }, false)
   el.addEventListener('touchend', function(e) {
+    const skewActivePlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' .card-planet')
+    const skewPrevPlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' .card-planet')
+    const skewNextPlanetCard = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' .card-planet')
+    skewActivePlanetCard.style.cssText = ''
+    skewPrevPlanetCard.style.cssText = ''
+    skewNextPlanetCard.style.cssText = ''
+    const skewActivePlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.activeIndex]  + ' video')
+    const skewPrevPlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.previousIndex]  + ' video')
+    const skewNextPlanet = document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' .planet-body') || document.querySelector('.' + mobileCarousel.planets[mobileCarousel.nextIndex]  + ' video')
+    skewActivePlanet.style.cssText = ''
+    skewPrevPlanet.style.cssText = ''
+    skewNextPlanet.style.cssText = ''
+
     const deltaX = swipe_det.eX - swipe_det.sX
     const deltaY = swipe_det.eY - swipe_det.sY
 
@@ -77,9 +123,9 @@ function detectSwipe(id, func, deltaMin = 90) {
     if (deltaX ** 2 + deltaY ** 2 < deltaMin ** 2) return
     // horizontal
     if (deltaY === 0 || Math.abs(deltaX / deltaY) > 1)
-      direction = deltaX > 90 ? directions.RIGHT : directions.LEFT
+      direction = deltaX > 0 ? directions.RIGHT : directions.LEFT
     else // vertical
-      direction = deltaY > 90 ? directions.UP : directions.DOWN
+      direction = deltaY > 0 ? directions.UP : directions.DOWN
 
     if (direction && typeof func === 'function') func(el, direction)
 
